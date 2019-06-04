73d674e7632eb1d9ef87efe00e3f0ab0268754a8
unknown <adielrr@gmail.com>
2019-06-04 19:05:54 -0400

    Ajuste para roteamento do modulo seguranca

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: 'login', component: LoginFormComponent},
 ];
 
 @NgModule({
--- src/app/seguranca/seguranca.module.ts
+++ src/app/seguranca/seguranca.module.ts
@@ -1,3 +1,4 @@
+import { SegurancaRoutingModule } from './seguranca-routing.module';
 import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
@@ -11,7 +12,8 @@ import {CardModule} from 'primeng/card';
     CommonModule,
     InputTextModule,
     CardModule,
-    FormsModule
+    FormsModule,
+    SegurancaRoutingModule
   ],
   declarations: [LoginFormComponent],
   exports:[LoginFormComponent]
